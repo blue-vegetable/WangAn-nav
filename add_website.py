2da5cada79a08379d216f40534328de48d68c868
wuruoye <2019302180149@whu.edu.cn>
2021-08-17 20:46:08 +0800

    modification

--- add_website.py
+++ add_website.py
@@ -26,16 +26,22 @@ def process_name_url(names, urls):
 #         'https://bkxk.whu.edu.cn/xtgl/login_slogin.html?language=zh_CN',
 #         'http://www.mooc.whu.edu.cn/portal', 'https://www.lib.whu.edu.cn/']
 
-type = "邮箱"
-id = "e-mails"
-names = ['武大邮箱', '微软邮箱', '网易邮箱', '谷歌邮箱', 'QQ邮箱']
-urls = ['https://ehall.whu.edu.cn/new/index.html', 'http://bkjw.whu.edu.cn/',
-        'https://bkxk.whu.edu.cn/xtgl/login_slogin.html?language=zh_CN',
-        'http://www.mooc.whu.edu.cn/portal', 'https://www.lib.whu.edu.cn/']
+# type = "邮箱"
+# id = "e-mails"
+# names = ['武大邮箱', '微软邮箱', '网易邮箱', '谷歌邮箱', 'QQ邮箱']
+# urls = ['https://email.whu.edu.cn/', 'https://outlook.live.com/mail',
+#         'https://mail.163.com/',
+#         'https://www.google.com/gmail/', 'https://mail.qq.com/']
 
+type = "学习"
+id = "study"
+names = ['GITHUB', 'LEETCODE', 'bilibili', '中国大学mooc', 'coursera']
+urls = ['https://github.com/', 'https://leetcode-cn.com/problemset/all/',
+        'https://www.bilibili.com/',
+        'https://www.icourse163.org/', 'https://www.coursera.org/']
 
 with open("./config/config_websites.json", "r", encoding='utf-8') as f:
     dic = json.load(f)
     add_website(dic, type, id, process_name_url(names, urls))
-with open("./config/config_websites2.json", "wt+", encoding='utf-8') as f:
+with open("./config/config_websites.json", "wt+", encoding='utf-8') as f:
     json.dump(dic, f, ensure_ascii=False, indent=4)
