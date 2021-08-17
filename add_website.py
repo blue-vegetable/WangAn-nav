dcfd5a7a1f22da8eb0f50d2f7c0f5a21192d5746
wuruoye <2019302180149@whu.edu.cn>
2021-08-17 19:47:44 +0800

    Update add_website.py

--- add_website.py
+++ add_website.py
@@ -1,5 +1,17 @@
 import json
 
+
+def add_website(dic, type, id, name, url):
+    for kind in dic['kinds']:
+        if kind['id'] == id:
+            kind['sites'].append({'name': name, 'Url': url})
+            break
+    else:
+        dic['kinds'].append(
+            {'type': type, 'id': id, 'sites': [{'name': name, 'Url': url}]})
+
+
 with open("./config/config_websites.json", "r") as f:
-    # dic = json.load(f)
-    print(f.read())
+    dic = json.load(f)
+    add_website(dic, '武大2', 'WHU2', '瞎写的', 'www.baidu.com')
+    print(dic)
